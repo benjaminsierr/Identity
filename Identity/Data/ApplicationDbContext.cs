ae52d4833661ff137ebc43eba87734154f119fcc
benjaminsierr <benjaminsierr@gmail.com>
2021-04-07 16:51:11 -0600

    Add project files.

---
+++ Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //public DbSet<Messages> Messages { get; set; }
+    }
+}
